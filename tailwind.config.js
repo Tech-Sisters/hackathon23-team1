9d189fd50d51fba9057330acc4bfe3ab183e43b7
zaaiiin <zaainnaseem@gmail.com>
2023-11-17 10:30:06 +0800

    add theme colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        turquoise: "#A3DDCB",
+        yellow: "#E8E9A1",
+        orange: "#E6B566",
+        pink: "#E5707E",
       },
     },
   },
   plugins: [],
-}
+};
