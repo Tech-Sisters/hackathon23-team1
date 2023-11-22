7f741bf01d38b2a16f7e23dad320d25d0ae3a44e
khodijah0498 <khodijahlawal@gmail.com>
2023-11-22 20:23:54 +0100

    feat: login-form

--- components/signup-form.js
+++ components/signup-form.js
@@ -1,6 +1,23 @@
 import React from 'react'
 
 const Signup = () => {
+  const [signPayload, setSignPayload] = useState({
+    name: "",
+    email: "",
+    password: "",
+    
+    
+  });
+
+  const [signupStage, setSignupStage] = useState(0);
+  const handleInputField = (e) => {
+    const { name, value } = e.target;
+    setSignPayload((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  
   return (
     <div>
         <Header/>
