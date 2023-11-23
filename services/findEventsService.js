95c3ec64f26060704ccabb8fe68d3514cc728a26
zaaiiin <zaainnaseem@gmail.com>
2023-11-23 10:00:49 +0800

    add margin to bottom of upcoming events

--- services/findEventsService.js
+++ services/findEventsService.js
@@ -179,31 +179,32 @@ const FindEventsService = () => {
           Miles of You:
         </h2>
       </div>
-
-      <div className="flex flex-wrap -mx-2 px-0  w-full">
-        {filteredEvents.map((event) => (
-          <div
-            key={event.id}
-            className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4  text-center mb-28"
-          >
-            <div className="bg-white p-0 m-2 border rounded-md cursor-pointer hover:shadow-lg border-slate-300 h-[300px]">
-              <Image
-                src={imagePaths[event.imageKey]}
-                alt="event-image"
-                width={300}
-                height={300}
-                className="w-[300px] h-[180px] rounded-t-sm"
-              />
-              <h3 className="text-lg font-bold  mt-3 text-pink text-left px-4">
-                {event.name}
-              </h3>
-              <div className="details pt-2 px-4 font-hind font-semibold text-left">
-                <p className="text-gray-800">{event.time}</p>
-                <p className="text-gray-700">{event.place}</p>
+      <div className="flex justify-left flex-col w-full mb-28">
+        <div className="flex flex-wrap -mx-2 px-0  w-full mb-28">
+          {filteredEvents.map((event) => (
+            <div
+              key={event.id}
+              className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4  text-center"
+            >
+              <div className="bg-white p-0 m-2 border rounded-md cursor-pointer hover:shadow-lg border-slate-300 h-[300px] ">
+                <Image
+                  src={imagePaths[event.imageKey]}
+                  alt="event-image"
+                  width={300}
+                  height={300}
+                  className="w-[300px] h-[180px] rounded-t-sm"
+                />
+                <h3 className="text-lg font-bold  mt-3 text-pink text-left px-4">
+                  {event.name}
+                </h3>
+                <div className="details pt-2 px-4 font-hind font-semibold text-left ">
+                  <p className="text-gray-800">{event.time}</p>
+                  <p className="text-gray-700">{event.place}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}{" "}
+        </div>
         <EventList events={events} imagePaths={imagePaths} country="Malaysia" />
         <EventList
           events={events}
