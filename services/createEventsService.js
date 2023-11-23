a998bedf6151f5e55cb64103494c9fa95c7659d9
zaaiiin <zaainnaseem@gmail.com>
2023-11-23 18:04:50 +0800

    add styles to create event page

--- services/createEventsService.js
+++ services/createEventsService.js
@@ -1,6 +1,7 @@
 import { React, useState } from "react";
 import Image from "next/image";
 import MeetupPhoto from "/public/images/meetup-photo.jpg";
+import MuslimWomen from "/public/images/muslim-women.jpg";
 
 const EventsForm = () => {
   const initialValues = {
@@ -10,16 +11,24 @@ const EventsForm = () => {
     organisingcommittee: "",
   };
 
-  const [formValues, setFormValues] = useState(initialValues);
+  const [eventFormValues, setEventFormValues] = useState(initialValues);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
+    console.log(e.target);
+    setEventFormValues({ ...eventFormValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
   };
   return (
-    <div className="create-events_container flex flex-wrap my-10 w-screen m-10">
-      <form className="events-form mx-auto p-7 py-20 border-r-white w-1/2 rounded-md shadow-md justify-left ">
-        <h1 className="text-center text-lg font-700 font-montserrat text-black ">
+    <div className="create-events_container flex flex-wrap my-8 mx-40 w-screen ">
+      <form
+        className="events-form mx-auto p-7 py-20 border-r-white w-1/2 rounded-md shadow-md justify-left relative z-10"
+        onSubmit={handleSubmit}
+      >
+        <h1 className="text-center text-2xl mb-10 mt-[-60px] font-700 font-montserrat text-black ">
           Create An Event
         </h1>
         <div className="mb-4">
@@ -29,7 +38,8 @@ const EventsForm = () => {
           <input
             type="text"
             id="name"
-            className="bg-orange appearance-none border-2 border-orange opacity-[15%] rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-pink"
+            onChange={handleChange}
+            className="bg-orange appearance-none border-1 border-orange bg-opacity-[15%] rounded-md w-full py-2 px-3 text-black leading-tight focus:outline-none focus:border-pink"
           />
         </div>
 
@@ -40,7 +50,8 @@ const EventsForm = () => {
           <input
             type="text"
             id="location"
-            className="bg-orange appearance-none border-2 border-orange opacity-30  rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-pink"
+            onChange={handleChange}
+            className="bg-orange appearance-none border-1 border-orange bg-opacity-[25%]  rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-pink"
           />
         </div>
 
@@ -51,7 +62,8 @@ const EventsForm = () => {
           <input
             type="text"
             id="time"
-            className="bg-orange appearance-none border-2 border-orange opacity-[15%]  rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-pink"
+            onChange={handleChange}
+            className="bg-orange appearance-none border-1 border-orange bg-opacity-[15%]  rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-pink"
           />
         </div>
 
@@ -65,26 +77,28 @@ const EventsForm = () => {
           <input
             type="text"
             id="organising-committee"
-            className="bg-orange appearance-none border-2 border-orange opacity-30  rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-pink"
+            onChange={handleChange}
+            className="bg-orange appearance-none border-1 border-orange bg-opacity-[25%]  rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-pink"
           />
         </div>
 
         <div className="flex items-center justify-center">
           <button
-            className="bg-orange hover:bg-yellow text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-orange hover:bg-yellow text-white font-bold py-2 px-4 rounded-3xl focus:outline-none focus:shadow-outline"
             type="button"
           >
             Submit
-          </button>
+          </button>{" "}
         </div>
       </form>
+
       <div className="flex flex-col w-1/2 rounded-md shadow-md align-top justify-center bg-orange">
         <Image
-          src={MeetupPhoto}
-          alt="event-image"
+          src={MuslimWomen}
+          alt="muslim-women"
           width={6855}
           height={6855}
-          className="w-[900px] h-[450px] py-0 mb-2 align-top "
+          className="w-[750px] h-[550px] rounded align-top "
         />
         <div className="text-center font-montserrat font-700  text-white text-2xl  align-bottom mt-5">
           Nurture a community
