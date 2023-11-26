62d05c974f470f7c12d29dd3c8d2d7bb168fd152
zaaiiin <zaainnaseem@gmail.com>
2023-11-26 12:22:41 +0800

    fix header

--- pages/signup-form.js
+++ pages/signup-form.js
@@ -1,7 +1,8 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import Input from "../components/input";
 import Header from "@/components/ui/header";
+import Layout from "@/components/ui/layout";
 
 const Signup = () => {
   const [signPayload, setSignPayload] = useState({
@@ -19,63 +20,65 @@ const Signup = () => {
   };
 
   return (
-    <div>
-      <Header />
-      <form className="signup-form mx-auto p-7 py-20 border-r-white w-1/2 rounded-md shadow-md justify-left relative z-10 mt-36">
-        <h1 className="text-center text-2xl mb-10 mt-[-60px] font-700 font-montserrat text-black ">
-          Create An Account
-        </h1>
+    <main className="flex h-screen flex-col items-center bg-yellow">
+      <Layout />
+      <section className=" bg-white justify-center flex flex-wrap mt-[105px]  h-full w-full ">
+        <form className="signup-form mx-auto p-7 border-r-white w-1/2 rounded-md shadow-md justify-center relative z-10 mt-20">
+          <h1 className="text-center text-2xl mb-10 mt-[-60px] font-700 font-montserrat text-black ">
+            Create An Account
+          </h1>
 
-        <div className="w-full flex flex-col gap-y-2 ">
-          <div className="flex flex-col gap-y-12">
-            <div className="flex gap-x-8">
-              <Input
-                placeholder="FullName"
-                name="fullName"
-                onChange={handleInputField}
-                value={signPayload.fullName}
-              />
-            </div>
+          <div className="w-full flex flex-col gap-y-2 ">
+            <div className="flex flex-col gap-y-12">
+              <div className="flex gap-x-8">
+                <Input
+                  placeholder="FullName"
+                  name="fullName"
+                  onChange={handleInputField}
+                  value={signPayload.fullName}
+                />
+              </div>
 
-            <div className="flex gap-x-8">
-              <Input
-                placeholder="Email"
-                name="email"
-                onChange={handleInputField}
-                value={signPayload.email}
-              />
-            </div>
+              <div className="flex gap-x-8">
+                <Input
+                  placeholder="Email"
+                  name="email"
+                  onChange={handleInputField}
+                  value={signPayload.email}
+                />
+              </div>
 
-            <div className="flex gap-x-8 mb-6">
-              <Input
-                type="password"
-                placeholder="PassWord"
-                name="password"
-                onChange={handleInputField}
-                value={signPayload.password}
-              />
+              <div className="flex gap-x-8 mb-6">
+                <Input
+                  type="password"
+                  placeholder="PassWord"
+                  name="password"
+                  onChange={handleInputField}
+                  value={signPayload.password}
+                />
+              </div>
+              <div className="flex gap-x-8 mb-6">
+                <Input
+                  type="location"
+                  placeholder="Location"
+                  name="addresss"
+                  onChange={handleInputField}
+                  value={signPayload.location}
+                />
+              </div>
             </div>
-            <div className="flex gap-x-8 mb-6">
-              <Input
-                type="location"
-                placeholder="Location"
-                name="addresss"
-                onChange={handleInputField}
-                value={signPayload.location}
-              />
+            <div className="flex items-center justify-center">
+              <button
+                className="bg-turquoise hover:bg-yellow text-white font-bold px-10 py-1 rounded-md mx-4 mt-4 h-10 focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Submit
+              </button>
             </div>
           </div>
-          <div className="flex items-center justify-center">
-            <button
-              className="bg-turquoise hover:bg-yellow text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-            >
-              Submit
-            </button>
-          </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </section>
+    </main>
   );
 };
 
