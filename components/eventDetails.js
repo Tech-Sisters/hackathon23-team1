ae3499cdbad259d7bf86a921eaa41d071fe9f4e0
zaaiiin <zaainnaseem@gmail.com>
2023-11-26 12:55:01 +0800

    add icons

--- components/eventDetails.js
+++ components/eventDetails.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import ClearIcon from "public/Images/clear-icon.png";
 import allImagePaths from "@/public/imagePaths";
+import DetailsIcon from "/public/Images/details-icon.png";
+import TimeIcon from "/public/Images/time-icon.png";
+import LocationIcon from "/public/Images/location-icon.png";
 
 const EventDetails = ({ event, onClose }) => {
   const [attending, setAttending] = useState(
@@ -57,9 +60,18 @@ const EventDetails = ({ event, onClose }) => {
             </button>
           </div>
           <h3 className="text-md font-semibold mb-2 mt-4 font-montserrat text-pink">
+            <div className="icon inline-flex items-center text-white font-hind font-bold rounded-md hover:opacity-70 text-md ">
+              <Image
+                src={DetailsIcon}
+                className=" inline-flex w-4 h-4 m-2"
+                alt="search-icon"
+                width={15}
+                height={15}
+              />
+            </div>{" "}
             Details
           </h3>
-          <p className="description text-gray-700 font-hind font-semibold">
+          <p className="description text-gray-700 font-hind font-semibold ml-3">
             {event.description} Lorem Ipsum is simply dummy text of the printing
             and typesetting industry. Lorem Ipsum has been the industry's
             standard dummy text ever since the 1500s, when an unknown printer
@@ -71,14 +83,35 @@ const EventDetails = ({ event, onClose }) => {
             publishing software like Aldus PageMaker including versions of Lorem
             Ipsum.
           </p>
+
           <h3 className="text-md font-bold mb-2 mt-8 font-montserrat text-pink">
+            <div className="icon inline-flex items-center text-white font-hind font-bold rounded-md hover:opacity-70 text-md ">
+              <Image
+                src={TimeIcon}
+                className=" inline-flex w-4 h-4 m-2"
+                alt="search-icon"
+                width={15}
+                height={15}
+              />
+            </div>{" "}
             Time
           </h3>
-          <p className="text-gray-700 font-hind font-semibold">{event.time}</p>
+          <p className="text-gray-700 font-hind font-semibold ml-3">
+            {event.time}
+          </p>
           <h3 className="text-md font-bold mb-2 mt-8  font-montserrat text-pink">
+            <div className="icon inline-flex items-center text-white font-hind font-bold rounded-md hover:opacity-70 text-md ">
+              <Image
+                src={LocationIcon}
+                className=" inline-flex w-4 h-4 m-2"
+                alt="search-icon"
+                width={15}
+                height={15}
+              />
+            </div>{" "}
             Location
           </h3>
-          <p className="text-gray-700 font-hind font-semibold">
+          <p className="text-gray-700 font-hind font-semibold ml-3">
             {event.place}, {event.country}
           </p>
         </div>
