991b8eb5abc8a16cce79147d03d1fe16234cb4d5
FabihaHassan123 <62029852+FabihaHassan123@users.noreply.github.com>
2023-11-28 14:53:51 +0000

    edit _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,3 +5,11 @@ import "@/styles/nav.css";
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+// export default function App({ Component, pageProps: { session, ...pageProps } }) {
+//   return (
+//     <SessionProvider session={session}>
+//       <Component {...pageProps} />
+//     </SessionProvider>
+//   );
+// }
